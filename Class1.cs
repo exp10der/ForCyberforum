6dc40302dd813218d9264a2fc8327b8884b72e92
Konstantin <alx0000@yandex.ru>
2015-03-26 14:26:11 +0300

    For Cyberforum

---
+++ Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+class Class1
+{
+    static void Main()
+    {
+        MainAsync().Wait();
+    }
+
+    static async Task MainAsync()
+    {
+        const string add = "https://api.vk.com/method/likes.getList?type=post&owner_id=135215546&item_id=4377&filter=likes&frinds_only=0&extended=1&offset=1&count=1000&access_token=aa5d63a10ecf232dd20ae58f807722d5e5a8d553e88fb7a833c8622bfe558ef472191341e75ddbfb8d182&item_id=";
+
+        string[] m = { "4377", "4247", "4390", "4241", "4275", "4274", "4392" };
+        var task = m.Select(async url =>
+        {
+            using (var clien = new HttpClient())
+            {
+                return await clien.GetStringAsync(add + url);
+            }
+
+        }).ToList();
+
+        var results = await Task.WhenAll(task);
+
+        var query = results.SelectMany(n => JObject.Parse(n)["response"]["items"].Select(x => x["uid"].ToString()))
+            .GroupBy(n => n, (k, n) => new {k, count = n.Count()}).OrderByDescending( n=>n.count);
+
+        foreach (var q in query)
+        {
+            Console.WriteLine(q.k + " " + q.count);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Diagnostics;
+//using System.Linq;
+//using System.Net.Http;
+//using System.Threading;
+//using System.Threading.Tasks;
+//using Newtonsoft.Json.Linq;
+
+//namespace jsonnet
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+
+//            //var mylist = new List<string>();
+//            //string[] strings = { "4377", "4247", "4390", "4241", "4275", "4274", "4392" };
+//            //var locker = new object();
+
+           
+//            //    Parallel.ForEach(strings, async str =>
+//            //    {
+
+//            //        using (HttpClient http = new HttpClient())
+//            //        {
+
+//            //            mylist.Add(
+//            //                await
+//            //                    http.GetStringAsync(
+//            //                        "https://api.vk.com/method/likes.getList?type=post&item_id=4377&filter=likes&frinds_only=0&extended=1&offset=1&count=1000&access_token=aa5d63a10ecf232dd20ae58f807722d5e5a8d553e88fb7a833c8622bfe558ef472191341e75ddbfb8d182&owner_id=1" +
+//            //                        str));
+//            //            Debug.Print("lol");
+//            //        }
+
+//            //        //lock (locker)
+//            //        //{
+//            //        //    JObject jObject = JObject.Parse(ss);
+//            //        //    mylist.AddRange(jObject["response"]["items"].Select(n => n["uid"].ToString()).ToList());
+//            //        //}
+//            //    });
+           
+            
+
+//            //string str;
+//            //using (HttpClient http = new HttpClient())
+//            //{
+//            //    str = http.GetStringAsync("http://vk.cc/3Ad3m8").Result;
+//            //}
+
+          
+
+
+//            Thread.Sleep(5000);
+//            //JObject jObject = JObject.Parse(str);
+
+//            //IList<string> allUid = jObject["response"]["items"].Select(n => n["uid"].ToString()).ToList();
+
+//            //foreach (var uid in allUid)
+//            //{
+//            //    Console.WriteLine(uid);
+//            //}
+           
+//            //Console.ReadKey(true);
+//        }
+//    }
+//}
